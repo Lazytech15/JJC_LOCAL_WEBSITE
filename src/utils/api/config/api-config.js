af499025bd351956fcf0fc88a30796bc77cfc0cf
StrawberryKeiyk <119676510+STRBRYKEIYK@users.noreply.github.com>
2025-09-25 15:27:31 +0800

    hi

--- src/utils/api/config/api-config.js
+++ src/utils/api/config/api-config.js
@@ -2,8 +2,8 @@
 // config/api-config.js
 // ============================================================================
 export const API_ENDPOINTS = {
-  local: "http://192.168.68.140:3001",
-  public: window.location.origin,
+  puplic: "http://192.168.68.140:3001",
+  //public: window.location.origin,
 }
 
 export const DEFAULT_HEADERS = {
