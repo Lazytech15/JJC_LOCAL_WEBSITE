6dfb6f45030f5c9eb7aedc93e4837106296fcdd7
emman <emmanuelablao16@gmail.com>
2025-11-01 13:27:34 +0800

    fixing api-config for adding the new endpoint url

--- src/utils/api/config/api-config.js
+++ src/utils/api/config/api-config.js
@@ -3,7 +3,7 @@
 // ============================================================================
 export const API_ENDPOINTS = {
 
-  public: "http://jjcenggworks.com" || "https://jjcenggworks.com",
+  public:"https://jjcenggworks.com",
 }
 
 export const DEFAULT_HEADERS = {
